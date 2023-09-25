58edb9b10a00746dce07a815a7ae7e4d088bf951
sho <shojabon@gmail.com>
2023-09-25 22:33:14 +0900

    Notionデータベースリンク作成

--- ScheduleAutoManager/__init__.py
+++ ScheduleAutoManager/__init__.py
@@ -1,5 +1,7 @@
 import json
 
+from pymongo import MongoClient
+
 from ScheduleAutoManager.data_class.GoogleCalendarManager import GoogleCalendarManager
 from ScheduleAutoManager.data_class.NotionManager import NotionManager
 
@@ -10,6 +12,8 @@ class ScheduleAutoManager:
         config_file = open("config/config.json")
         self.config = json.loads(config_file.read())
         config_file.close()
+
+        self.mongo = MongoClient(self.config["mongodb"])
 
         self.notion_manager = NotionManager(self)
         self.google_calendar_manager = GoogleCalendarManager(self)
--- ScheduleAutoManager/data_class/NotionManager.py
+++ ScheduleAutoManager/data_class/NotionManager.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
+from notion_client import Client
 
 if TYPE_CHECKING:
     from ScheduleAutoManager import ScheduleAutoManager
@@ -8,4 +9,43 @@ if TYPE_CHECKING:
 class NotionManager:
 
     def __init__(self, main: ScheduleAutoManager):
-        pass
+        self.main = main
+        self.notion = Client(auth=main.config["notion"]["apiKey"])
+        self.database_id = main.config["notion"]["databaseId"]
+
+        self.update_database()
+
+    def update_database(self, start_from: str = None, page_size: int = 10):
+        query_result = self.notion.databases.query(
+            database_id=self.database_id,
+            sorts=[
+                {
+                    "property": "最終更新日時",
+                    "direction": "descending"
+                }
+            ],
+            start_cursor=start_from,
+            page_size=page_size,
+        )
+
+        results = query_result["results"]
+
+        updated_tasks = []
+        last_task = None
+        for task in results:
+            # upsert to mongodb
+            mongo_push_result = self.main.mongo["scheduleAutoManager"]["notion_tasks"].update_one({"id": task["id"]},
+                                                                                                  {"$set": task},
+                                                                                                  upsert=True).raw_result
+            if mongo_push_result["nModified"] == 1:
+                updated_tasks.append(True)
+            elif "upserted" in mongo_push_result:
+                updated_tasks.append(True)
+            else:
+                updated_tasks.append(False)
+
+            last_task = task
+
+        # if last page is updated, recursively update
+        if len(results) == page_size and updated_tasks[-1]:
+            self.update_database(start_from=last_task["id"], page_size=page_size*2)
