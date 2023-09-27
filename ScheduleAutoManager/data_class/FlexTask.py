40200a1d2efd6632a295df5099fd5c1ba5181841
sho <shojabon@gmail.com>
2023-09-27 18:13:40 +0900

    flextask基本プロパティ追加 flex task取得メソッド追加

--- ScheduleAutoManager/data_class/FlexTask.py
+++ ScheduleAutoManager/data_class/FlexTask.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+
+import datetime
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -10,3 +12,22 @@ class FlexTask:
     def __init__(self, data: dict, main: ScheduleAutoManager):
         self.data = data
         self.main = main
+
+    def get_id(self):
+        return self.data["id"]
+
+    def get_name(self) -> str:
+        return str(self.data["properties"]["名前"]["title"][0]["text"]["content"])
+
+    def get_date_data(self):
+        return self.data["properties"]["日付"]["date"]
+
+    def get_start_date(self):
+        if self.get_date_data()["start"] is None:
+            return None
+        return datetime.datetime.fromisoformat(self.get_date_data()["start"])
+
+    def get_end_date(self):
+        if self.get_date_data()["end"] is None:
+            return self.get_start_date()
+        return datetime.datetime.fromisoformat(self.get_date_data()["end"])
--- ScheduleAutoManager/manager/NotionManager.py
+++ ScheduleAutoManager/manager/NotionManager.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 from notion_client import Client
 
+from ScheduleAutoManager.data_class.FlexTask import FlexTask
+
 if TYPE_CHECKING:
     from ScheduleAutoManager import ScheduleAutoManager
 
@@ -13,7 +15,11 @@ class NotionManager:
         self.notion = Client(auth=main.config["notion"]["apiKey"])
         self.database_id = main.config["notion"]["databaseId"]
 
-        # self.update_database()
+        self.task_cache = {}
+
+        self.update_database()
+
+        print(self.get_task("0610ea52-d1b6-409f-8589-0812d9c77d95").get_date())
 
     def update_database(self, start_from: str = None, page_size: int = 10):
         query_result = self.notion.databases.query(
@@ -37,15 +43,34 @@ class NotionManager:
             mongo_push_result = self.main.mongo["scheduleAutoManager"]["notion_tasks"].update_one({"id": task["id"]},
                                                                                                   {"$set": task},
                                                                                                   upsert=True).raw_result
-            if mongo_push_result["nModified"] == 1:
+            if mongo_push_result["nModified"] == 1 or "upserted" in mongo_push_result:
                 updated_tasks.append(True)
-            elif "upserted" in mongo_push_result:
-                updated_tasks.append(True)
+                if task["id"] in self.task_cache:
+                    del self.task_cache[task["id"]]
             else:
                 updated_tasks.append(False)
-
             last_task = task
+
+        print(updated_tasks)
 
         # if last page is updated, recursively update
         if len(results) == page_size and updated_tasks[-1]:
             self.update_database(start_from=last_task["id"], page_size=page_size*2)
+
+    def get_task(self, task_id: str) -> FlexTask | None:
+        if task_id in self.task_cache:
+            return self.task_cache[task_id]
+        task = self.main.mongo["scheduleAutoManager"]["notion_tasks"].find_one({"id": task_id})
+        if task is None:
+            return None
+        task_object = FlexTask(task, self.main)
+        self.task_cache[task_id] = task_object
+        return self.task_cache[task_id]
+
+    def get_all_tasks(self) -> list[FlexTask]:
+        result = []
+        task_ids = self.main.mongo["scheduleAutoManager"]["notion_tasks"].find({}, {"id": 1})
+        for task_id in task_ids:
+            result.append(self.get_task(task_id["id"]))
+        return result
+
